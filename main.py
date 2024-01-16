3d011150f93f2213bc860f40a6181ace3d7a4d78
Eduardo Velazco <eduardo@evelazco.com>
2024-01-16 23:39:33 +0100

    Carga de los primeros archivos de prueba

---
+++ main.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.ticker as mticker
+
+def main():
+    st.title("Dashboard de Población Mundial")
+if __name__ == "__main__":
+    main()
